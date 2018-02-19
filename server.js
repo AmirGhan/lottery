7d735cc9e2035b8d305dca98a3c1e0d7659ef468
Amir <amir.ghandeharioon@gmail.com>
2018-02-19 01:59:53 +0000

    routes added

--- server.js
+++ server.js
@@ -1,2 +1,34 @@
 const express = require('express');
+const app = express();
+const PORT = 3000;
 const game = new (require('./src/game'))();
+
+app.get("/", function(req, res) {
+  res.send("Hellooooo")
+});
+
+app.post("/purchase", function(req, res) {
+  let ticket = game.purchase("Amir");
+  console.log("Purchased By: " + ticket.name + " , Ticket number: " + ticket.num + ", Price: " + ticket.val);
+});
+
+app.post("/draw", function(req, res) {
+  let winnablePot = game.draw()
+  console.log("Winnable Value of the Pot: $" + winnablePot);
+});
+
+app.post("/winners", function(req, res) {
+  let results = game.result();
+  console.log(results)
+});
+
+app.post("/restart", function(req, res) {
+  game.restart();
+
+});
+
+
+
+app.listen(PORT, () => {
+  console.log("Lottery app listening on port " + PORT);
+});
